b8cc2e271f7ff912518c4540777cf0eed4e898a9
Emmanuel <emmanuelugwueze6@gmail.com>
2022-08-15 23:03:33 +0100

    drafts ui chnages

--- lib/views/desktop_view/user_drafts.dart
+++ lib/views/desktop_view/user_drafts.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:genesys_blog/constant.dart';
+import 'package:genesys_blog/controllers/all_providers/all_providers.dart';
+import 'package:genesys_blog/controllers/drafts_controller.dart';
 import 'package:genesys_blog/views/desktop_view/users_dashboard.dart';
 import 'package:genesys_blog/widgets/category.dart';
 import 'package:genesys_blog/widgets/widgets.dart';
@@ -29,29 +31,55 @@ class _UserDraftsState extends ConsumerState<UserDrafts> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    DraftsController _draftProvider = ref.read(draftsProvider);
   }
 
   @override
   Widget build(BuildContext context) {
+      DraftsController _draftProvider = ref.watch(draftsProvider);
     return SingleChildScrollView(
         child: Padding(
       padding: const EdgeInsets.only(left: 44.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+      child: _draftProvider.load? Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Gap(100),
+                CircularProgressIndicator(
+                  color: darkBlueColor,
+                ),
+              ],
+            ))
+          : Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Gap(20),
         const DashboardMetricsWidget(
           width: 391,
           boxType: 'Drafts',
           imagePath: 'assets/post.png',
           mertricNumber: '10',
-        ),Gap(70),
-        Text('All Drafts',
-              style: GoogleFonts.poppins(
-                  color: black, fontSize: 20, fontWeight: FontWeight.w400),),Gap(15),
-        ListView.separated(
+        ),
+        Gap(70),
+        Text(
+          'All Drafts',
+          style: GoogleFonts.poppins(
+              color: black, fontSize: 20, fontWeight: FontWeight.w400),
+        ),
+        Gap(15),
+         _draftProvider.draftsList!.isEmpty? Column(
+                                        children: [
+                                          Image.asset(
+                                              'assets/nothing-found.png',
+                                              height: 300),
+                                          Text('You have no Drafts',
+                                              style: GoogleFonts.poppins(
+                                                  fontSize: 24,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: black)),
+                                        ],
+                                      )
+                                    :  ListView.separated(
             shrinkWrap: true,
-            itemCount: 3,
+            itemCount:  _draftProvider.draftsList!.length,
             separatorBuilder: (context, int) {
               return const Divider();
             },
