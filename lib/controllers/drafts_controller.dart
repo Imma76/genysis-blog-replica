ed17c6624021515f859cabec95e68b821ded3b69
Emmanuel <emmanuelugwueze6@gmail.com>
2022-08-15 23:00:32 +0100

    drafts controllerw

--- lib/controllers/drafts_controller.dart
+++ lib/controllers/drafts_controller.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:genesys_blog/models/drafts_model.dart';
+import 'package:genesys_blog/services/drafts_service.dart';
 
 class DraftsController extends ChangeNotifier {
-  List<DraftsModel>? draftsList;
-  Future getDrafts()async{
-    
+  List<DraftsModel?>? draftsList;
+  bool load = false;
+  Future getDrafts() async {
+    DraftsService _draftService = DraftsService();
+
+    try {
+      load = true;
+      draftsList = await _draftService.getUserDrafts();
+      load = false;
+      notifyListeners();
+    } catch (e) {
+       load = false;
+      notifyListeners();
+    }
   }
 }
