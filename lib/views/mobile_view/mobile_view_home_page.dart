1dd2fd31a699f1b0b738a37cd8c2febf03183191
Emmanuel <emmanuelugwueze6@gmail.com>
2022-09-07 22:32:14 +0100

    mobile navigations

--- lib/views/mobile_view/mobile_view_home_page.dart
+++ lib/views/mobile_view/mobile_view_home_page.dart
@@ -34,160 +34,167 @@ class _MobileViewHomePageState extends ConsumerState<MobileViewHomePage> {
     _userController.init();
   }
 
-  
   @override
   Widget build(BuildContext context) {
     HomePageController _homePageProvider = ref.watch(homePageProvider);
-     UserController _userController = ref.watch(userProvider);
+    UserController _userController = ref.watch(userProvider);
     return Scaffold(
-      key: _homePageProvider.key,
-
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: darkBlueColor),
-        elevation: 0.0,
-        title: Text(
-          'Genesys Blog',
-          style: GoogleFonts.poppins(
-            fontSize: 20.sp,
-            fontWeight: FontWeight.w600,
-            color: white,
+        key: _homePageProvider.key,
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: darkBlueColor),
+          elevation: 0.0,
+          title: Text(
+            'Genesys Blog',
+            style: GoogleFonts.poppins(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w600,
+              color: white,
+            ),
           ),
+          backgroundColor: darkBlueColor,
         ),
-        backgroundColor: darkBlueColor,
-      ),
-      drawer:  Drawer(child:Center(
+        drawer: Drawer(
+          child: Center(
             child: Padding(
               padding: EdgeInsets.all(8.0),
-              child:_userController.firstName == null ?Column(children: [
-                 GestureDetector(
-                        onTap: () async {
-                          Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return MobileSignIn();
-                        }));
-                        },
-                        child: ListTile(
-                          title: Text('Sign In',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.red)),
-                        )),
-              ],):Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                              child: CachedNetworkImage(
-                                  imageUrl: _userController.photo.toString())),
-                          const Gap(20),
-                          Column(
-                            children: [
-                              Text(_userController.firstName.toString(),
+              child: _userController.firstName == null
+                  ? Column(
+                      children: [
+                        GestureDetector(
+                            onTap: () async {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return MobileSignIn();
+                              }));
+                            },
+                            child: ListTile(
+                              title: Text('Sign In',
                                   style: GoogleFonts.poppins(
-                                      fontSize: 20,
+                                      fontSize: 14,
                                       fontWeight: FontWeight.w400,
-                                      color: black)),
-                              const Gap(5),
-                              Text(_userController.email.toString(),
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w400,
-                                      color: black)),
-                            ],
+                                      color: Colors.red)),
+                            )),
+                      ],
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Center(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircleAvatar(
+                                    child: CachedNetworkImage(
+                                        imageUrl:
+                                            _userController.photo.toString())),
+                                const Gap(20),
+                                Column(
+                                  children: [
+                                    Text(_userController.firstName.toString(),
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w400,
+                                            color: black)),
+                                    const Gap(5),
+                                    Text(_userController.email.toString(),
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w400,
+                                            color: black)),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
-                    ),
-                    const Gap(20),
-                    GestureDetector(
-                        onTap: () {
-                       ///   _homePageProvider.changeIndex(0);
-                        },
-                        child: ListTile(
-                          title: Text('Home',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: black)),
-                        )),
-                    const Gap(15),
-                    GestureDetector(
-                        onTap: () {
-                         // _homePageProvider.changeIndex(3);
-                        },
-                        child: ListTile(
-                          title: Text('Dashboard',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: black)),
-                        )),
-                    const Gap(15),
-                    GestureDetector(
-                        onTap: () {
-                          // Navigator.push(context,
-                          //     MaterialPageRoute(builder: (context) {
-                          //   return const UserPost();
-                          // }));
-                          _homePageProvider.changeIndex(1);
-                        },
-                        child: ListTile(
-                          title: Text('Posts',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: black)),
-                        )),
-                    const Gap(15),
-                    GestureDetector(
-                        onTap: () {
-                          // Navigator.push(context,
-                          //     MaterialPageRoute(builder: (context) {
-                          //   return const UserDrafts();
-                          // }));
-                          _homePageProvider.changeIndex(2);
-                        },
-                        child: ListTile(
-                          title: Text('Drafts',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: black)),
-                        )),
-                    const Gap(15),
-                    GestureDetector(
-                        onTap: () {
-                          BotToast.showText(text: "xxxx");
-                        },
-                        child: ListTile(
-                          title: Text('Profile settings',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: black)),
-                        )),
-                    const Gap(15),
-                    GestureDetector(
-                        onTap: () async {
-                          await _userController.signOut();
-                          Navigator.pop(context);
-                        },
-                        child: ListTile(
-                          title: Text('Sign out',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.red)),
-                        )),
-                  ]),
+                          const Gap(20),
+                          GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+
+                                ///   _homePageProvider.changeIndex(0);
+                              },
+                              child: ListTile(
+                                title: Text('Home',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: black)),
+                              )),
+                          const Gap(15),
+                          GestureDetector(
+                              onTap: () {
+                                 _homePageProvider.changeIndex(2);
+                              },
+                              child: ListTile(
+                                title: Text('Dashboard',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: black)),
+                              )),
+                          const Gap(15),
+                          GestureDetector(
+                              onTap: () {
+                                // Navigator.push(context,
+                                //     MaterialPageRoute(builder: (context) {
+                                //   return const UserPost();
+                                // }));
+                                _homePageProvider.changeIndex(1);
+                              },
+                              child: ListTile(
+                                title: Text('Posts',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: black)),
+                              )),
+                          const Gap(15),
+                          GestureDetector(
+                              onTap: () {
+                                // Navigator.push(context,
+                                //     MaterialPageRoute(builder: (context) {
+                                //   return const UserDrafts();
+                                // }));
+                                _homePageProvider.changeIndex(2);
+                              },
+                              child: ListTile(
+                                title: Text('Drafts',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: black)),
+                              )),
+                          const Gap(15),
+                          GestureDetector(
+                              onTap: () {
+                                BotToast.showText(text: "xxxx");
+                              },
+                              child: ListTile(
+                                title: Text('Profile settings',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: black)),
+                              )),
+                          const Gap(15),
+                          GestureDetector(
+                              onTap: () async {
+                                await _userController.signOut();
+                                Navigator.pop(context);
+                              },
+                              child: ListTile(
+                                title: Text('Sign out',
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.red)),
+                              )),
+                        ]),
             ),
-          ),),
-      body:_homePageProvider.mobileWidgetList[_homePageProvider.currentIndex]
-    );
+          ),
+        ),
+        body:
+            _homePageProvider.mobileWidgetList[_homePageProvider.currentIndex]);
   }
 }
